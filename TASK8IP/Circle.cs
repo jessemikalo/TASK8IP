508d6d405da046b78ad104536f7c49384a489ffc
jesse <jesse@JESSE>
2022-10-04 12:41:54 +0300

    Add project files.

---
+++ TASK8IP/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK8IP
+{
+    internal class Circle
+    {
+        //Constructor:
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        //Properties: starting with upper case letter!
+        public double Radius { get; set; }
+
+        //Instance methods:
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        public override string ToString()
+        {
+            //return base.ToString();
+            return "Radius is " + String.Format("{0:F2}", Radius);
+        }
+
+    }
+}
---
+++ TASK8IP/Program.cs
@@ -0,0 +1,30 @@
+﻿using TASK8IP;
+
+bool stillMore;
+double cRadius;
+
+do
+{
+    Console.Write("Please, enter radius: ");
+    string received = Console.ReadLine();
+    while (!Double.TryParse(received, out cRadius) || cRadius<0)
+    {
+        Console.Write("Not valid, try again: ");
+        received = Console.ReadLine();
+    }
+    //Now you can create an object of type Circle:
+    //Call your constructor:
+    Circle circle=new Circle(cRadius);
+
+    //Console.WriteLine("Area of the circle is {0:F3} square units.", circle.CalculateArea());
+    //Console.WriteLine("Circumference is {0:F3}.", circle.CalculateCircumference());
+    
+
+
+    Console.Write("More of this (Y/N): ");
+    string decision = Console.ReadLine().ToUpper();
+    if (decision.StartsWith("Y"))
+        stillMore = true;
+    else
+        stillMore = false;
+} while (stillMore);
